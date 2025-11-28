11547bc61da5ce226ddbafddbe158dec52d050cc
BertrandDecoster <70576987+BertrandDecoster@users.noreply.github.com>
2025-11-28 21:31:57 +0100

    Logger_Win fixed so that  trace works on windows

--- src/FXPlatform/Win/Logger_Win.cpp
+++ src/FXPlatform/Win/Logger_Win.cpp
@@ -8,14 +8,51 @@
 
 #include "Logger.h"
 #include "Windows.h"
+#include <memory>
+#include <fstream>
+
+static std::unique_ptr<std::ofstream> g_logFile = nullptr;
+
+// Function pointer for trace callback (defined by PythonInterface.cpp)
+typedef void (*TraceCallback)(const char* message);
+
+// Declare the function to get callback from PythonInterface
+extern "C" TraceCallback GetTraceCallback();
 
 // Implementation of debug output on windows
 void DebugLogMessage(int traceType, const TraceDetail levelOfDetail, const char *message)
 {
-	OutputDebugString(message);
+	// Get callback dynamically from PythonInterface
+	TraceCallback callback = GetTraceCallback();
+
+	// Use callback if set, otherwise default to OutputDebugString
+	if(callback) {
+		callback(message);
+	} else {
+		OutputDebugString(message);
+	}
+
+	// Also output to file if enabled
+	if(g_logFile && g_logFile->is_open()) {
+		(*g_logFile) << message;
+		g_logFile->flush();
+	}
 }
 
 
 void DebugLogMessagesToFile(const std::string &filename)
 {
+	// Close existing file if open
+	if(g_logFile) {
+		g_logFile.reset();
+	}
+
+	// If filename is provided, open new file
+	if(filename.size() > 0) {
+		g_logFile = std::make_unique<std::ofstream>(filename, std::ios::out | std::ios::trunc);
+		if(g_logFile->is_open()) {
+			(*g_logFile) << "Begin Logging\n";
+			g_logFile->flush();
+		}
+	}
 }
